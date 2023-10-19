b00dd3398952379896eb4e75080e384de7b5b80f
C-o-m-o-n <comon928@gmail.com>
2023-10-19 13:41:26 +0300

    add county agent page

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, url_for
 
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='static',
+            template_folder='templates')
 
 @app.route('/')
 def home():
     return render_template('index.html', name="home")
+
+@app.route('/county-agents')
+def county_agents():
+    return render_template('county-agents.html', name="county-agents")
 
 @app.route('/login')
 def login():
